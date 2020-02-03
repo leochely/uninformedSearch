b4f82b8726b73a71a6d507be611649bf5b577591
Léo Chely <31514354+leochely@users.noreply.github.com>
2020-02-02 20:16:26 -0700

    Distance printed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys, queue
+from collections import defaultdict
 
-def find_path(path, origin_city, destination_city):
+def find_path(graph, path, origin_city, destination_city):
     current_city = destination_city
     path_final = []
     while current_city is not origin_city:
@@ -9,7 +10,7 @@ def find_path(path, origin_city, destination_city):
     path_final.append(origin_city)
     path_final.reverse()
     for i in range(len(path_final)-1):
-        print(path_final[i], " to ", path_final[i+1])
+        print(path_final[i], " to ", path_final[i+1], ", ", graph[path_final[i]][path_final[i+1]], "km")
 
 file_name = sys.argv[1]
 
@@ -19,21 +20,14 @@ destination_city = sys.argv[3]
 file = open(file_name,"r")
 lines = file.readlines()
 
-graph = {}
-
+graph = defaultdict(dict)
 for line in lines[:-2]:
     city0 = line.split()[0]
     city1 = line.split()[1]
     distance = int(line.split()[2])
 
-    if city0 in graph:
-        graph[city0].append((city1, distance))
-    else:
-        graph[city0] = [(city1, distance)]
-    if city1 in graph:
-        graph[city1].append((city0, distance))
-    else:
-        graph[city1] = [(city0, distance)]
+    graph[city0][city1] = distance
+    graph[city1][city0] = distance
         
 visited = []
 path = {}
@@ -43,18 +37,17 @@ queue.put((0, (origin_city, origin_city)))
 total_distance = 0
 
 path[origin_city] = None
-while queue:
+while not queue.empty():
     distance, (city, previous_city) = queue.get()
     if city not in visited:
         path[city] = previous_city
         visited.append(city)
         if city == destination_city:
             print('route found ', distance)
-            
-            find_path(path, origin_city, city)
-            exit(0)
+            find_path(graph, path, origin_city, city)
+            raise Exception('exit')
         else:
-            for next_city in graph[city]:
+            for next_city in graph[city].items() :
                 if next_city[0] not in visited:
                     new_distance = distance + next_city[1]
                     queue.put((new_distance, (next_city[0], city)))
