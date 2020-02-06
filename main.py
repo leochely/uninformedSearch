ef374edd2325ace6415a9279fe06e77236ba9901
Koolaznkid <dan@mymail.mines.edu>
2020-02-05 22:56:09 -0700

    Updated output to correctly output when target city is not found

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ def find_path(graph, path, origin_city, destination_city):
     path_final.append(origin_city)
     path_final.reverse()
     for i in range(len(path_final)-1):
-        print(path_final[i], 'to', path_final[i+1], ',', graph[path_final[i]][path_final[i+1]], 'km')
+        string1 = path_final[i]
+        string2 = path_final[i+1]
+        distance = graph[path_final[i]][path_final[i+1]]
+        print(string1.strip(), "to", string2.strip(), ",", distance, "km")
+        
 
 file_name = sys.argv[1]
 
@@ -36,6 +40,9 @@ queue.put((0, (origin_city, origin_city)))
 
 total_distance = 0
 
+#This variable is used to notify that the target city has been found or not
+cityFound = 0;
+
 path[origin_city] = None
 while not queue.empty():
     distance, (city, previous_city) = queue.get()
@@ -46,11 +53,17 @@ while not queue.empty():
             print('distance:', distance, 'km')
             print('route:')
             find_path(graph, path, origin_city, city)
+
+            #Update cityFound to indicate the target city is found
+            cityFound = 1;
         else:
             for next_city in graph[city].items() :
                 if next_city[0] not in visited:
                     new_distance = distance + next_city[1]
                     queue.put((new_distance, (next_city[0], city)))
-print('distance:infinity')
-print('route:')
-print('none')
+
+#Print out the city was never found, if there was no path to it from the source
+if cityFound == 0:
+    print('distance:infinity')
+    print('route:')
+    print('none')
