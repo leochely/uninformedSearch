a150b7c8874ca1acb51e7bb2f97b22e4e4110bc1
Koolaznkid <dan@mymail.mines.edu>
2020-02-05 22:37:55 -0700

    Modified main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def find_path(graph, path, origin_city, destination_city):
     path_final.append(origin_city)
     path_final.reverse()
     for i in range(len(path_final)-1):
-        print(path_final[i], " to ", path_final[i+1], ", ", graph[path_final[i]][path_final[i+1]], "km")
+        print(path_final[i], 'to', path_final[i+1], ',', graph[path_final[i]][path_final[i+1]], 'km')
 
 file_name = sys.argv[1]
 
@@ -40,16 +40,17 @@ path[origin_city] = None
 while not queue.empty():
     distance, (city, previous_city) = queue.get()
     if city not in visited:
-        print(city)
         path[city] = previous_city
         visited.append(city)
         if city == destination_city:
-            print('route found ', distance)
+            print('distance:', distance, 'km')
+            print('route:')
             find_path(graph, path, origin_city, city)
-            raise Exception('exit')
         else:
             for next_city in graph[city].items() :
                 if next_city[0] not in visited:
                     new_distance = distance + next_city[1]
                     queue.put((new_distance, (next_city[0], city)))
-print('route not found')
+print('distance:infinity')
+print('route:')
+print('none')
